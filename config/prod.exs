1b6dfd61b9c615f97f79080b0f7fb8e5026a9b98
iDavi <odavi20527@gmail.com>
2025-01-05 20:21:31 -0300

    (feat) prod config file

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,11 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :kacy_api, KacyApiWeb.Endpoint,
+  http: [port: 3000],
+  url: [host: "kacychat.site", port: 3000]
+
+config :kacy_api, :cors_origins, ["https://kacychat.site", "https://www.kacychat.site"]
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
